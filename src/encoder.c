08e6885fedf1ab8e0d1f862a5b2956f5d221b020
bx <bx@debian>
2018-04-01 09:01:45 +0200

    encoder stuff #3

--- src/encoder.c
+++ src/encoder.c
@@ -9,26 +9,38 @@ static int encoder_value;
 static int encoder_direction;
 
 int encoder_init() {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
- 
-    GPIO_InitTypeDef gpio_cfg;
-    gpio_cfg.GPIO_Mode = GPIO_Mode_IPU;
-    gpio_cfg.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-    gpio_cfg.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(GPIOA, &gpio_cfg);
- 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
- 
-    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
-    TIM_TimeBaseStructInit(&TIMER_InitStructure);
-    TIMER_InitStructure.TIM_Period = 100;
-    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up | TIM_CounterMode_Down;
-    TIM_TimeBaseInit(TIM5, &TIMER_InitStructure);
- 
-    TIM_EncoderInterfaceConfig(TIM5, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
- 
-    TIM_Cmd(TIM5, ENABLE);
-	return 0;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_ICInitTypeDef TIM_ICInitStructure;
+
+	TIM_DeInit(TIM2);
+	TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	TIM_TimeBaseStructure.TIM_Period = 0xFFFF;
+	TIM_TimeBaseStructure.TIM_Prescaler = 0;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+	TIM_ICStructInit(&TIM_ICInitStructure);
+	TIM_ICInitStructure.TIM_ICFilter = 0x00;
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+
+	TIM_EncoderInterfaceConfig(TIM2, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+
+	TIM2->CNT = 0;
+
+	TIM_Cmd(TIM2, ENABLE);
+	return;
 }
 
 int encoder_read() {
