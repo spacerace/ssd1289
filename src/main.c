4b84d55c7b83da4e9ecc45641bfcdb6491e01651
Nils Stec <stecdose@gmail.com>
2018-06-23 09:21:52 +0200

    ...

--- src/main.c
+++ src/main.c
@@ -2,25 +2,25 @@
  * SSD1289 driver example
  * Nils Stec <stecdose@gmail.com>
  * 2018-03-29
- * 
+ *
  * This driver was written to be used with or without an OS.
  * This is the FreeRTOS example on how to use it.
- * 
+ *
  * The whole thing was written using a board called
  * "HY-MiniSTM32V" with an LCD called "HY32D".
  * It's specs:
  *   - STM32F103VCT6, 256k flash, 64k RAM
  *   - display connection 8080 bus to FSMC
- * 
+ *
  * There are a three tasks running:
  * 		vT_display_demo()	- all you can see on screen when demo is running
- *      vT_shell()			- a simple console running on USART1, 
+ *      vT_shell()			- a simple console running on USART1,
  * 							  interfacing all graphics+demo-functions
  *      vT_led()			- blinks a led and checks buttonX
- * 
+ *
  * There is also an ISR installed for a button connected to PC13.
  * It's purpose is to cycle through the demo screens.
- *  
+ *
  */
 
 #include <stdio.h>
@@ -58,7 +58,7 @@ uint32_t dwt_get_cycle_cnt();
 void dwt_reset_cycle_cnt();
 
 volatile uint32_t *DWT_CONTROL = (uint32_t *)0xE0001000;
-volatile uint32_t *DWT_CYCCNT = (uint32_t *)0xE0001004; 
+volatile uint32_t *DWT_CYCCNT = (uint32_t *)0xE0001004;
 volatile uint32_t *DEMCR = (uint32_t *)0xE000EDFC;
 
 
@@ -68,7 +68,7 @@ volatile uint32_t *DEMCR = (uint32_t *)0xE000EDFC;
 void vT_shell(void *p) {
 	init_console();
 	for(;;) {
-		console_main();		
+		console_main();
 	}
 }
 
@@ -76,16 +76,16 @@ void vT_encoder(void *p) {
 	ssd1289_set_font(FONT_LINUX_8x16);
 	ssd1289_set_font_color(RGB_COL_YELLOW, RGB_COL_BLACK);
 	ssd1289_set_text_cursor(0,0);
-	
+
 	for(;;) {
-		
+
 	}
 }
 
 int main(void){
     char str[32];
     uint32_t start, end, ms;
-    
+
  	SystemInit();
 	usart1_init();
 	init_leds();
@@ -96,26 +96,26 @@ int main(void){
 	ssd1289_set_font(FONT_LINUX_8x16);
 
 	init_buttons();
-	encoder_init();	
+	encoder_init();
 
     LED1_OFF();
     LED2_OFF();
-    
+
 	systick_counter = 0;
-    
+
     dwt_enable();
 	/* 1000 interrupts per second = 1khz */
 // 	SysTick_Config(SystemCoreClock/1000);
-    
-    
 
-    
+
+
+
 // 	int i,color;
 // 	color = LCD_COLOR(0, 0, 255);
 // 	for(i = 0; i < 5; i++) {
 // 		ssd1289_dotmatrix_digit(i*42, 10, 13, LCD_COLOR(0x1a, 0x2a, 0x3a));
 // 		ssd1289_dotmatrix_digit(i*42, 10, i, color<<=3);
-// 	}		
+// 	}
 
     ssd1289_print_image(&mi, 0, 0);
 
@@ -124,17 +124,17 @@ int main(void){
     start = dwt_get_cycle_cnt();
 
     printf("Hello World\r\n");
-    
+
     end = dwt_get_cycle_cnt();
     ms = end-start;
-    
+
     uint32_t time_used = ms*78;
     time_used /= 10;
-    
+
     sprintf(str, "%lunS", time_used);
     ssd1289_puts_at(10, 150, str);
 
-    
+
 // 	xTaskCreate(vT_shell,   	 (const char*) "Shell Task", 256, NULL, 1, NULL);
 // 	xTaskCreate(vT_led,     	 (const char*) "LED Task", 48, NULL, 1, NULL);
 // 	xTaskCreate(vT_display_demo, (const char*) "SSD1289_DEMO", 256, NULL, 1, NULL);
@@ -145,9 +145,9 @@ int main(void){
 
 
 	for(;;) {
-		
+
 	}
-		
+
 	return 0;
 }
 
@@ -157,7 +157,7 @@ void dwt_enable() {
 }
 
 void dwt_enable_cycle_cnt() {
-*DWT_CONTROL = 0x40000001;
+	*DWT_CONTROL = 0x40000001;
 }
 
 void dwt_disable_cycle_cnt() {
